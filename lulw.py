6013a787e1c19010c8918dea5e1810f458a87c38
wowfilip999 <67916726+wowfilip999@users.noreply.github.com>
2021-10-19 16:41:11 +0000

    added multi function
    single for scan one port
    added "open on" "closed on"

--- lulw.py
+++ lulw.py
@@ -2,25 +2,48 @@ a = 0
 import time
 import socket
 s = socket.socket()
-ip = input("ip: ")
-portstart = input("from : ")
-portend = input("to : ")
-delay = input("delay y/n/custom : " )
-if delay == "custom":
-  custm = input("custom delay : ")
 
-for a in range(int(portstart), int(portend)):
-   try:
-       s.connect((ip, int(a)))
-       s.close()
-       status = "open"
-   except:
-       status = "closed"
 
-   print("port " + str(a)  + "" +  status)
-   if delay == "y":
-     time.sleep(0.7)
-   if delay == "n":
-     pass
-   if delay == "custom":
-     time.sleep(int(custm))
+def single():
+ ip = input("ip : ")
+ port = input("port : ")
+
+ try:
+     s.connect((ip, int(port)))
+     status = "open"
+ except:
+     status = "closed"
+
+ print(str(port)  +  status + " on " +  str(ip))
+def multi():
+ ip = input("ip : ")
+ portstart = input("from : ")
+ portend = input("to : ")
+ delay = input("delay y/n/custom : " )
+ if delay == "custom":
+   custm = input("custom delay : ")
+
+ for a in range(int(portstart), int(portend)):
+    try:
+        s.connect((ip, int(a)))
+        s.close()
+        status = "open"
+    except:
+           status = "closed"
+
+    print("port " +  str(a)   +  status + " on " + str(ip))
+    if delay == "y":
+      time.sleep(0.7)
+    if delay == "n":
+      time.sleep(0)
+    if delay == "custom":
+      time.sleep(int(custm))
+
+
+
+while True:
+  mode = input("multi/single : ")
+  if mode == "multi":
+    multi()
+  if mode == "single":
+    single()
